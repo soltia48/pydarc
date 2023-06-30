84d8e5e82f56e99d872335e5de699e0a9d7a6a57
soltia48 <soltia48@gmail.com>
2023-06-30 09:58:49 +0900

    Add a type hint and a comment

--- pydarc/lfsr.py
+++ pydarc/lfsr.py
@@ -1,4 +1,13 @@
-def lfsr(seed: int, polynomial: int):
+def lfsr(seed: int, polynomial: int) -> int:
+    """Galois Linear-Feedback Shift Register
+
+    Args:
+        seed (int): Seed
+        polynomial (int): Polynomial
+
+    Yields:
+        int: Bit (0 or 1)
+    """
     state = seed
     while True:
         lsb = state & 1
